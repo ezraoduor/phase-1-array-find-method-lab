c85951fea2e8ae1f1d9e1fa15c902bd15b06ca64
Ezra Oduor <ezrachurchill20@gmail.com>
2025-03-11 09:01:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record){
+    const year = record.find(item => item.year === "2015")
+    if(year){
+        return year.year
+    }else{
+        return undefined
+    }
+}
